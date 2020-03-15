3f6990014d677ea149528086f035d1ac62584d45
gabrieldev525 <gvictor525.gv@gmail.com>
2020-03-15 16:11:36 -0300

    Improved match algorithm

--- utils.py
+++ utils.py
@@ -1,81 +1,90 @@
-def match_people(person, list_people):
-    current_match = None
+from constants import WORD_MATCH_SCORE
+from constants import ABREVIATION_MATCH_SCORE
 
-    for people in list_people:
+
+def match_people(person, list_people):
+    """
+        SCORES:
+            - For each word match +2 points in the match score
+            - For each word with abreviation match +1 point in the match score
+    """
+
+    for compare_people in list_people:
         match_score = 0
         max_score = 0
 
         # perffect data
-        if people[1].lower() == person[1].lower():
-            match_score += 10
-        elif people[2].lower() == person[2].lower():
-            match_score += 10
-        elif people[3].lower() == person[3].lower():
-            match_score += 10
-        elif people[4].lower() == person[4].lower():
-            match_score += 10
-        elif people[5].lower() == person[5].lower():
-            match_score += 10
+        # check each name information, name, mother name, father name, genre, birth date
+        for i in range(1, 5):
+            if compare_people[i].lower() == person[i].lower():
+                match_score += len(person[1].split(' ')) * WORD_MATCH_SCORE
 
-        max_score += 10
+            max_score += len(person[1].split(' ')) * WORD_MATCH_SCORE
 
         # anomaly
 
-        #### name ####
-        people_from_list_names = people[1].split(' ')
+        # ### name ### #
+        people_from_list_names = compare_people[1].split(' ')
         person_names = person[1].split(' ')
 
-        match_score += match_name(person_names, people_from_list_names)
-        max_score = 5 * len(person_names) * len(people_from_list_names)
+        match_result = match_name(person_names, people_from_list_names)
+        match_score += match_result[0]
+        max_score += match_result[1]
 
-        #### mom name ####
-        mom_name_from_list_names = people[2].split(' ')
+        # ### mom name ### #
+        mom_name_from_list_names = compare_people[2].split(' ')
         mom_name = person[2].split(' ')
 
-        match_score += match_name(mom_name, mom_name_from_list_names)
-        max_score = 5 * len(mom_name) * len(mom_name_from_list_names)
+        match_result = match_name(mom_name, mom_name_from_list_names)
+        match_score += match_result[0]
+        max_score += match_result[1]
 
-        #### father name ####
-        dad_from_list_names = people[3].split(' ')
+        # ### father name ### #
+        dad_from_list_names = compare_people[3].split(' ')
         dad_names = person[3].split(' ')
 
-        match_score += match_name(dad_names, dad_from_list_names)
-        max_score = 5 * len(dad_names) * len(dad_from_list_names)
-
-        import ipdb; ipdb.set_trace()
+        match_result = match_name(dad_names, dad_from_list_names)
+        match_score += match_result[0]
+        max_score += match_result[1]
 
         # verify based in score if it is the same people
-        percent_from_max = (max_score * 86) / 100
+        percent_from_max = (max_score * 50) / 100
+        # import ipdb; ipdb.set_trace()
         if match_score > percent_from_max:
-            return people
+            import ipdb; ipdb.set_trace()
+            return compare_people
 
     return None
 
 
 def match_name(person_names, people_from_list_names):
     match_score = 0
+    max_score = 0
+
     for name in person_names:
         for to_match_name in people_from_list_names:
-            # if it is not an Abbreviation
-            if len(name) > 1 and len(to_match_name) > 1:
+            # none is abreviation
+            if(len(name) > 1 and len(to_match_name) > 1):
                 if name.lower() == to_match_name.lower():
-                    match_score += 5
-            elif len(name) == 1:
-                # Abbreviation
+                    match_score += WORD_MATCH_SCORE
+                max_score += WORD_MATCH_SCORE
+
+            # the person name is a abreviation
+            elif(len(name) == 1 and len(to_match_name) > 1):
                 if name.lower() == to_match_name[0].lower():
-                    match_score += 2
-            elif to_match_name.lower() == name[0].lower():
-                    match_score += 2
+                    match_score += ABREVIATION_MATCH_SCORE
+                max_score += ABREVIATION_MATCH_SCORE
 
-            # Do the match by letter
-            letters_name = list(name.lower())
-            letter_to_match = list(to_match_name.lower())
+            # the word to compare is a abreviation
+            elif(len(name) > 1 and len(to_match_name) == 1):
+                if name[0].lower() == to_match_name.lower():
+                    match_score += ABREVIATION_MATCH_SCORE
+                max_score += ABREVIATION_MATCH_SCORE
 
-            match_letters = list(filter(lambda x : x in letters_name, letter_to_match))
-            count = len(match_letters)
+            # the words are abreviations
+            else:
+                if name[0].lower() == to_match_name[0].lower():
+                    match_score += ABREVIATION_MATCH_SCORE
+                max_score += ABREVIATION_MATCH_SCORE
 
-            percent = (len(letter_to_match) * 100) / len(letters_name)
-            if percent > 85:
-                match_score += 5
-
-    return match_score
+    return match_score, max_score
